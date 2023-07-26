bafc0b4352d01c73aae3c3fc95fa0b7f5a8c0f81
sebeyes <seb.lhoumeau@gmail.com>
2023-07-26 16:05:46 +0100

    Test functions with real SLAM data

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,5 @@
 using BioExplorer
-using DataFrames
+using DataFrames, CSV
 
 # Test community matrix
 com_mat = (
@@ -10,18 +10,38 @@ com_mat = (
 )
 com_mat = DataFrame(com_mat, [:sp1, :sp2, :sp3, :sp4, :sp5, :sp6, :sp7])
 
+# Real community data (From SLAM TER 2022)
+SLAM_mat = CSV.File(
+    "test_community_matrix.csv",
+    delim = ";"
+) |> DataFrame
+
+community_list = names(select(SLAM_mat, Not(:MF)))
+species_list = SLAM_mat.MF
+
+SLAM_mat = permutedims(SLAM_mat)
+SLAM_mat = SLAM_mat[2:end,:]
+rename!(SLAM_mat, species_list)
+SLAM_mat.community = community_list
+SLAM_mat = select(SLAM_mat, Not(:NPI))
+describe(SLAM_mat)
+
+
 # Test hill series computation
 BioExplorer.hill(com_mat)
+BioExplorer.hill(select(SLAM_mat, Not(:community)))
 
 # Test rank computation
 BioExplorer.rank(com_mat[1,:])
 
 # Test whittacker_plot
 BioExplorer.whittacker_plot(com_mat[2,:])
+BioExplorer.whittacker_plot(SLAM_mat[34,Not(:community)])
 
 # Test octave
 BioExplorer.octave(com_mat[1,:])
 BioExplorer.octave(com_mat[2,:])
 
 # Test octave plot
-BioExplorer.octave_plot(Community)
+BioExplorer.octave_plot(com_mat[1,:])
+BioExplorer.octave_plot(SLAM_mat[40,Not(:community)])

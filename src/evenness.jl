9d9ffb5f0084bfc50bc38bf422cbded3d23586bd
SebEyes <seb.lhoumea@gmail.com>
2024-04-01 09:52:14 +0000

    Documenting Pielou eveness function

--- src/evenness.jl
+++ src/evenness.jl
@@ -1,3 +1,22 @@
+"""
+    pielou_evenness(community_matrix::Community_Matrix)
+
+Compute an evenness diversity metric according to the Pielou's framework.
+
+# Arguments
+- `community_matrix::Community_Matrix`: A community matrix containing species abundance data.
+
+# Returns
+- A DataFrame containing the Pielou's evenness (J) metric for each community.
+
+# Details
+The function calculates the Pielou's evenness (J) metric for each community within the given community matrix. 
+Pielou's evenness measures the equitability of species abundance distribution within a community. 
+It is calculated as the Shannon-Wiener entropy divided by the maximum possible entropy, which is the logarithm of the species richness. 
+
+# Reference 
+Pielou, E. C. (1969). An Introduction to Mathematical Ecology. Wiley-Interscience.
+"""
 function pielou_evenness(community_matrix::Community_Matrix)
 
     _checkType_mat_com_(community_matrix, "abundance")

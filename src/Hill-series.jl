bb6f476e94741fdd9eeeed97080de2576bb597fb
SebEyes <seb.lhoumea@gmail.com>
2024-04-01 09:42:27 +0000

    Documenting hill function

--- src/Hill-series.jl
+++ src/Hill-series.jl
@@ -1,4 +1,23 @@
-hill = function(community_matrix::BioExplorer.Community_Matrix)
+"""
+    hill(community_matrix::Community_Matrix)
+
+Compute the Hill series for all communities within a community matrix.
+
+# Arguments
+- `community_matrix::Community_Matrix`: A community matrix containing species abundance data for multiple communities.
+
+# Returns
+- A DataFrame representing the Hill series for each community, including Hill numbers H0, H1, H2, and H3.
+
+# Details
+The function calculates the Hill series, which represents the equivalent number of species at different orders of Hill number, for each community within the given community matrix. 
+Hill numbers measure biodiversity by scaling species abundance to a common unit, providing a way to compare biodiversity across different communities. 
+The output DataFrame contains the Hill numbers H0, H1, H2, and H3 for each community.
+
+# Reference
+Hill, M. O. (1973). Diversity and Evenness: A Unifying Notation and Its Consequences. Ecology, 54(2), 427–432. https://doi.org/10.2307/1934352
+"""
+function hill(community_matrix::BioExplorer.Community_Matrix)
 
     _checkType_mat_com_(community_matrix, "abundance")
 

04675ba9fd4da20844752a8e9842464b38ced990
SebEyes <seb.lhoumea@gmail.com>
2024-04-01 10:25:35 +0000

    Documenting species accumulation function

--- src/SAC.jl
+++ src/SAC.jl
@@ -1,7 +1,23 @@
 """
-    SAC()
+    SAC(community_matrix::Community_Matrix, npermut::Int64 = 0)
 
-Compute species accumulation curve for a given community matrix.
+Compute the species accumulation curve for a community matrix.
+
+# Arguments
+- `community_matrix::Community_Matrix`: The input community matrix.
+- `npermut::Int64`: The number of permutations for computing confidence intervals around the mean accumulation curve. Defaults to 0, indicating no permutation.
+
+# Returns
+- If `npermut` is 0, returns a vector representing the species accumulation curve.
+- If `npermut` is greater than 0, returns a DataFrame containing the mean accumulation curve and confidence intervals.
+
+- Makie Figure representing the species accumulation curve. 
+
+# Details
+The function computes the species accumulation curve for the input community matrix. 
+If `npermut` is greater than 0, it performs permutations of the community matrix and computes multiple accumulation curves to estimate confidence intervals around the mean accumulation curve. 
+The function generates a plot of the accumulation curve and displays it.
+
 """
 function SAC(community_matrix::Community_Matrix, npermut::Int64 = 0)
     com = collect(1:size(community_matrix.sites)[1])

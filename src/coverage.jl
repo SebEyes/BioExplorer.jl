fea5424b5bfaac442ab11ff586b81f311bdfd75a
SebEyes <seb.lhoumea@gmail.com>
2024-04-01 09:48:29 +0000

    Documenting sampling coverage function

--- src/coverage.jl
+++ src/coverage.jl
@@ -1,3 +1,22 @@
+"""
+    sampling_coverage(community_matrix::Community_Matrix)
+
+Compute the percentage of sampling coverage according to the Chao framework.
+
+# Arguments
+- `community_matrix::Community_Matrix`: A community matrix containing species abundance data.
+
+# Returns
+- The percentage of sampling coverage.
+
+# Details
+The function calculates the percentage of sampling coverage, which estimates the proportion of total species in the community that have been sampled. 
+It uses the Chao framework, which considers the abundance of rare species to estimate the coverage.
+The output represents the percentage of species that have been sampled based on the observed data.
+
+# Reference
+Chao, A., Kubota, Y., Zelený, D., Chiu, C., Li, C., Kusumoto, B., Yasuhara, M., Thorn, S., Wei, C., Costello, M. J., & Colwell, R. K. (2020). Quantifying sample completeness and comparing diversities among assemblages. Ecological Research, 35(2), 292–314. https://doi.org/10.1111/1440-1703.12102
+"""
 function sampling_coverage(community_matrix::Community_Matrix)
     _checkType_mat_com_(community_matrix, "abundance")
     species_ab = sum(community_matrix.species_data, dims = 1)

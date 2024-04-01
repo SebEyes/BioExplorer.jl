bbe3dae01d2fa63284dbd889ee7f960d9cddf19b
SebEyes <seb.lhoumea@gmail.com>
2024-04-01 10:08:46 +0000

    Documenting Gower distance functions

--- src/Gower_distance.jl
+++ src/Gower_distance.jl
@@ -1,3 +1,24 @@
+"""
+    pairwise_Gowdis(trait_matrix::Trait_Matrix, weight, species1::String, species2::String)
+
+Compute the Gower distance between a pair of species within a trait matrix.
+
+# Arguments
+- `trait_matrix::Trait_Matrix`: A trait matrix containing species trait data.
+- `weight`: Vector of weight parameters for each traits in the trait matrix. Can be missing, in this case, each traits are consider equals.
+- `species1::String`: The name of the first species.
+- `species2::String`: The name of the second species.
+
+# Returns
+- The Gower distance between the two species.
+
+# Details
+The function computes the Gower distance between a pair of species based on their trait data within the given trait matrix. 
+Gower distance is a measure of dissimilarity that accounts for different types of variables (categorical, numerical, and ordinal) and their respective weights.
+The dissimilarity is calculated for each trait variable and then combined using the provided weights to obtain the final Gower distance.
+
+See also [`matrix_Gowdis`](@ref)
+"""
 function pairwise_Gowdis(trait_matrix::Trait_Matrix, weight, species1::String, species2::String)
 
     #Security for trqit type encoding
@@ -103,7 +124,23 @@ function pairwise_Gowdis(trait_matrix::Trait_Matrix, weight, species1::String, s
     dissim
 end
 
+"""
+    matrix_Gowdis(trait_matrix::Trait_Matrix, weight)
 
+Compute the Gower distance between every pair of species within a trait matrix.
+
+# Arguments
+- `trait_matrix::Trait_Matrix`: A trait matrix containing species trait data.
+- `weight`: Vector of weight parameters for each traits in the trait matrix. Can be missing, in this case, each traits are consider equals.
+
+# Returns
+- A matrix representing the Gower distance between every pair of species.
+
+# Details
+The function computes the Gower distance between every pair of species based on their trait data within the given trait matrix. It iterates over all combinations of species and computes the pairwise Gower distance using the `pairwise_Gowdis` function. The resulting matrix represents the Gower distance between every pair of species.
+
+See also [`pairwise_Gowdis`](@ref)
+"""
 function matrix_Gowdis(trait_matrix::Trait_Matrix, weight)
 
     Gowdis = []
